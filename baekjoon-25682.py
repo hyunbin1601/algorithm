a35b742ee60949ac6b572513154ad8d38ebdde88
hyum's house <109196568+hyunbin1601@users.noreply.github.com>
2024-12-05 13:43:41 +0900

    # edit

--- baekjoon-25682.py
+++ baekjoon-25682.py
@@ -3,13 +3,15 @@ input = sys.stdin.readline
 
 # https://www.acmicpc.net/problem/25682
 # 누적 합 문제
+# python3에서 자꾸 시간초과 에러가 난다면 pypy3에서 실행해보자
+# pypy3는 jit compiler를 사용하기 때문에 좀 더 빠른 실행가능
+# pypy와 python은 대부분 호환되지만 일부 c 모듈은 호환되지 않을 수 있음주의
 
 n, m, k = map(int, input().split())
 
 chess = []
 
-for _ in range(n):
-    chess.append(input().strip())  # 체스판의 색깔 정보를 입력받음 
+chess = [input().strip() for _ in range(n)] 
     
 # 체스판의 각 칸이 체스판 규칙에 맞지 않는 경우 -> 누적 합 배열에 저장
 # 체스판의 규칙은 무조건 B와 W가 번갈아가면서 나와야함
@@ -19,12 +21,14 @@ sum_w = [[0] * (m + 1) for _ in range(n+1)] # W로 시작하는 체스판의 누
 
 for i in range(1, n+1):
     for j in range(1, m+1):
+        add_b = 0  # 현재 칸을 B로 시작하는 경우 추가 값
+        add_w = 0  # 현재 칸을 W로 시작하는 경우 추가 값
         if (i+j) % 2 == 0:
             if chess[i-1][j-1] == 'B':  # 체스칸이 B로 시작하는 경우
-                sum_b[i][j] = sum_b[i-1][j] + sum_b[i][j-1] - sum_b[i-1][j-1]  # 체스칸이 B로 시작하는 경우의 고쳐야할 블록이 저장된 누적합은 그대로
-                sum_w[i][j] = sum_w[i-1][j] + sum_w[i][j-1] - sum_w[i-1][j-1] + 1 # 체스칸이 w로 시작하는 경우의 고쳐야할 블록이 저장된 누적합은 1 증가 
+                sum_b[i][j] = sum_b[i-1][j] + sum_b[i][j-1] - sum_b[i-1][j-1]
+                sum_w[i][j] = sum_w[i-1][j] + sum_w[i][j-1] - sum_w[i-1][j-1] + 1  # w로 시작하는 체스칸을 수정해야함
             else: # 체스칸이 W로 시작하는 경우
-                sum_b[i][j] = sum_b[i-1][j] + sum_b[i][j-1] - sum_b[i-1][j-1] + 1
+                sum_b[i][j] = sum_b[i-1][j] + sum_b[i][j-1] - sum_b[i-1][j-1] + 1 # b로 시작하는 체스칸을 수정해야함
                 sum_w[i][j] = sum_w[i-1][j] + sum_w[i][j-1] - sum_w[i-1][j-1]
 
         else:
